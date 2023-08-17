8132fe0296b83a09d751b0d5e12e998d3ecfec14
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-08-17 11:35:38 -0700

    design login.page

--- login.php
+++ login.php
@@ -7,18 +7,22 @@ include('./navbar.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="allcss.css" rel="stylesheet" type="text/css"/>
 </head>
-<body>
+<body class="bdlogin">
+
+        <span class="spslogin"> Loging </span>
+
     <div>
     <form action="script.php" method="post">
-        <lable> Name </lable>
-        <input type ="text" name="name"> 
-        <lable> Password </lable>
+        <lable class=" lablogin"> Name </lable>
+        <input type ="text" name="name"> <br> <br>
+        <lable class="lablogin"> Password </lable>
         <input type ="password" name="password">  
 </form>
 </div>
 
-        <a href ="addnew.php"><button> Login </button> </a>
+        <a href ="addnew.php"><button class="bulogin"> Login </button> </a>
 
 </body>
 </html>
