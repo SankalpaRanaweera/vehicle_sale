4e26182cdaf22e5c66dfe0f7243a6c0184853e41
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-08-18 19:41:59 -0700

    added new pages

---
+++ bus.php
@@ -0,0 +1,103 @@
+<?php include('./navbar.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="allcss.css" rel="stylesheet" type="text/css">
+</head>
+<body bgcolor="gray">
+<div class="divfirst" >
+    <div class="div1moter">
+        <div class="diva">
+        <div> <img src="a.png " alter="a" class="img" > </div>
+       <div> <span> Contact </span> <strong> : </strong><span> 0771234567</span></div>
+       <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+       <div> <span> Location </span><strong> : </strong><span> 0771234567</span></div>
+       <div> <span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+       <div><span> Name </span><strong> : </strong><span> 0771234567</span></div>
+       <div><span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div> </div>
+  
+             
+    <div class="div1moter">
+    <div class="divb">  
+    <div> <img src="a.png " alter="b" class="img" > </div>
+    <div> <span> Contact </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+    <div><span> Location </span><strong> : </strong><span> 0771234567</span></div>
+    <div><span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+    <div><span> Name </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div> </div>
+                
+    <div class="div1moter">
+    <div class="divc">
+    <div> <img src=" a.png" alter="c" class="img" > </div>
+    <div> <span> Contact </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Location </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Name </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div></div>
+    <div class="div1moter">
+        <div class="divd">
+        <div> <img src="a.png " alter="d" class="img" > </div>
+       <div> <span> Contact </span><strong> : </strong><span> 0771234567</span></div>
+       <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+       <div> <span> Location </span><strong> : </strong><span> 0771234567</span></div>
+       <div> <span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+       <div><span> Name </span><strong> : </strong><span> 0771234567</span></div>
+       <div><span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div> </div>
+</div>               
+<div class="divsecond">
+    <div class="div2moter">
+    <div class="dive">
+    <div> <img src=" a.png" alter="d" class="img"> </div>
+    <div> <span> Contact </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Location </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Name </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div> </div>
+    
+    <div class="div2moter">
+    <div class="divf">
+    <div> <img src="a.png " alter="e" class="img"> </div>
+    <div> <span> Contact </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+    <div><span> Location </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+    <div><span> Name </span><strong> : </strong><span> 0771234567</span></div>
+    <div><span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div> </div>
+    
+    <div class="div2moter">
+    <div class="divg">
+    <div> <img src=" a.png" alter="f"class="img"> </div>  
+    <div> <span> Contact </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Location </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Name </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div>  </div> 
+    <div class="div2moter">
+    <div class="divh">
+    <div> <img src=" a.png" alter="d" class="img"> </div>
+    <div> <span> Contact </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Price</span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Location </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> YOM </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Name </span><strong> : </strong><span> 0771234567</span></div>
+    <div> <span> Milage </span><strong> : </strong><span> 0771234567</span></div>
+    </div> </div>
+    
+</div>            
+              
+</body>
+</html>
