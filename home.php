18345821ae45537b7d8479935bce408b84bdfd81
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-09-26 08:09:29 -0700

    add some part

--- home.php
+++ home.php
@@ -7,13 +7,14 @@
     <title>Document</title>
     <link href="allcss.css" rel="stylesheet" type="text/css"/>
 </head>
-<body class ="bd" bgcolor ="white"> 
+<body class ="bd"> 
     <div class ="s">
     <strong> Find & Sell Your Vehicle </strong>
 </div>
     <div>
         <img src ="60cce45fb495d.jpeg" class="img1">
 </div>
+<div class="h">
     <a href ="car.php"> <button class ="b1">CAR </button> </a>
     <a href="bus.php"> <button class ="b1">BUS </button> </a>
     <a href="van.php"><button class ="b1">VAN </button></a>
@@ -21,6 +22,7 @@
     <a href="threeweel.php"> <button class ="b1">THREE WHEELS </button> </a>
     <a href="lorry.php"><button class ="b1">LORRY </button> </a>
 </div>
+
     
 </body>
 </html>
