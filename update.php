510a9d6287572bd70e884fd008bb554b75bb0c89
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2025-04-28 01:05:13 -0700

    add some parts

--- update.php
+++ update.php
@@ -23,26 +23,26 @@
         $sql="SELECT * FROM vehicle_details WHERE nic='$nic' ";
         $res = mysqli_query ($con,$sql);
         if(mysqli_num_rows($res)>0){
-            $row=mysqli_fetch_assoc($res);
+            $row=mysqli_fetch_assoc($res);    //while($span=mysqli_fetch_assoc($res)){
             ?>
         <lable> Contact</lable>
-        <input type="text" name="contact" class="tx0addnew"><br>
+        <input type="text" name="contact" class="tx0addnew" value=<?php echo $row['contact'] ?> ><br>
         <lable> Price</lable>
-        <input type="text" name="price" class="tx1addnew"><br>
+        <input type="text" name="price" class="tx1addnew" value=<?php echo $row['price'] ?> ><br>
         <lable> Location</lable>
-        <input type="text" name="location" class="tx2addnew"><br>
+        <input type="text" name="location" class="tx2addnew" value=<?php echo $row['location'] ?> ><br>
         <lable>Type</lable>
-        <input type="text" name="type" class="tx3addnew"><br>
+        <input type="text" name="type" class="tx3addnew" value=<?php echo $row['type'] ?> ><br>
         <lable>Name</lable>
-        <input type="text" name="name" class="tx4addnew"><br>
+        <input type="text" name="name" class="tx4addnew" value=<?php echo $row['name'] ?> ><br>
         <lable>NIC</lable>
-        <input type="text" name="nic" class="tx7addnew"><br>
+        <input type="text" name="nic" class="tx7addnew" value=<?php echo $row['nic'] ?> ><br>
         <lable>Milage</lable>
-        <input type="text" name="milage" class="tx5addnew"><br>
+        <input type="text" name="milage" class="tx5addnew" value=<?php echo $row['milage'] ?> ><br>
         <lable>Add Image</lable>
-        <input type="file" name="image" class="tx6addnew">
+        <input type="file" name="image" class="tx6addnew" value=<?php echo $row['image'] ?> >
         <div class="divbuaddnew">
-        <input type="submit" name="upload" class="buaddnew">
+        <input type="submit" name="upload" class="buaddnew" >
     </div>
     <?php
         }
