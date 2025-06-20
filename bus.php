3fd9e080506333202fea46f22ab5e73b126f711b
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2025-06-19 20:35:57 -0700

    add some parts

--- bus.php
+++ bus.php
@@ -12,11 +12,11 @@
 <div class="div1moter">
 <?php
         $sql= "SELECT * from vehicle_details where type='bus'";
-        $res = mysqli_query($con,$sql);
+        $res = mysqli_query($con,$sql); 
         if(mysqli_num_rows($res)>0){
             while($span=mysqli_fetch_assoc($res)){
         echo"<div class='diva'>";
-        echo "<div> <img src='.$span[addimage]png ' alter='a' class='img' > </div>";
+        echo "<div> <img src='.$span[addimage] ' alter='a' class='img' > </div>";
         echo "<div> <span> Contact </span> <strong> : </strong> '<span>'".$span['contact']. "'</span>';</div>";
         echo "<div> <span> Price</span><strong> : </strong> '<span>'".$span['price']. "'</span>';</div>";
         echo "<div> <span> Location </span><strong> : </strong>'<span>'".$span['location']."'</span>';</div>";
